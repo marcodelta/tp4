a6302ebd71f8b2c8657efe6fb3a2b5475007aafc
raphaelb1 <raphael.beaziz@gmail.com>
2018-05-18 16:56:27 +0200

    Ajout des exceptions

---
+++ tp4/src/tp4/FondInexistant.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp4;
+
+/**
+ *
+ * @author rapha
+ */
+public class FondInexistant extends Exception {
+    
+    public String message;
+    
+    public FondInexistant(String m)
+    {
+        message=m;
+    }
+    
+   
+            
+
+            
+}
