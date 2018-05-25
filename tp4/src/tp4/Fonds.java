69d83481f499d4571499d7690ac83eb2e3af800e
Marc Rahbani <marcrahbani@hotmail.fr>
2018-05-25 09:47:54 +0200

    no message

--- tp4/src/tp4/Fonds.java
+++ tp4/src/tp4/Fonds.java
@@ -24,5 +24,28 @@ public Fonds() {
 public Fonds(double a) {
     amount=a;
 }
+
+   public int compareTo(Fonds t) {
+  
+    if(this.amount > t.amount)
+    {return 1;}
+    else if( this.amount == t.amount )
+    {return 0;}
+    else
+    {return -1; }
+      
+    }
+    
+    public boolean equals(Fonds t){
+        
+  
+     if(this.amount == t.amount)
+     return true  ;  
+     else
+             
+    return false;
+}
+
+      
     
 }
--- tp4/src/tp4/Instrument.java
+++ tp4/src/tp4/Instrument.java
@@ -14,14 +14,21 @@ import java.util.*;
 public class Instrument {
     
 public ArrayList<Fonds> collect;
+public Fonds f;
 
 public Instrument() {
     collect = new ArrayList<Fonds>();
+    f = new Fonds();
+    
 }
 
 public void ajoutfond(Fonds f){
     collect.add(f);
 }
 
+public void trier(Fonds f,Object obj)
+{
+       // Collections.sort(collect,f.compareTo(f));
 
+}
 }
